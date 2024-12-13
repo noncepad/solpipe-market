127139c2bcf0a10f5f3ca0dd4c3cc74a078ae4b0
Joel De Jesus <joel@noncepad.com>
2024-12-13 12:36:23 +0900

    fixed protobuf

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
-# Protobufs for Common Markets
+#!/bin/bash
 
+set -euo pipefail
 
-
-# Build Libraries
-
-## Go
-
-
-```bash
 protoc --experimental_allow_proto3_optional --proto_path=./txproc/proto --go-grpc_out=. --go_out=. $(ls ./txproc/proto)
 protoc --experimental_allow_proto3_optional --proto_path=./geyser/proto --go-grpc_out=. --go_out=. $(ls ./geyser/proto)
 protoc --experimental_allow_proto3_optional --proto_path=./solpipe --go-grpc_out=. --go_out=. $(ls ./solpipe)
@@ -15,6 +9,3 @@ protoc --experimental_allow_proto3_optional --proto_path=./catscope/proto --go-g
 protoc --experimental_allow_proto3_optional --proto_path=./bot/proto --go-grpc_out=. --go_out=. $(ls ./bot/proto)
 rsync -avz github.com/noncepad/solpipe-market/go/ ./go/
 rm -r github.com
-```
-
-
