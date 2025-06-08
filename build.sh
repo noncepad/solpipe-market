e386796a8fc17eaccfdb47cf83e2a96e76e604df
Joel De Jesus <joel@noncepad.com>
2025-06-08 12:51:32 +0900

    update catscope protobuf

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ protoc --experimental_allow_proto3_optional --proto_path=./geyser/proto --go-grp
 protoc --experimental_allow_proto3_optional --proto_path=./solpipe --go-grpc_out=. --go_out=. $(ls ./solpipe)
 protoc --experimental_allow_proto3_optional --proto_path=./catscope/proto --go-grpc_out=. --go_out=. $(ls ./catscope/proto)
 protoc --experimental_allow_proto3_optional --proto_path=./bot/proto --go-grpc_out=. --go_out=. $(ls ./bot/proto)
+rm -rf ./go/proto 2>/dev/null || true
 rsync -avz github.com/noncepad/solpipe-market/go/ ./go/
 rm -r github.com
